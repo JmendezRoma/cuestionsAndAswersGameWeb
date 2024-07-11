cf7db36d41b1a0a5cdaf73cab3bf6dacaeabc47e
Jonathan <jona_pana13@hotmail.com>
2024-07-11 10:21:08 +0100

    se suben los ficheros basicos al repositorio

---
+++ js/parseJSON.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+const url = '../JSON/quiz.json'
+
+function getAndParseJSON() {
+    
+    fetch(url)
+
+        .then((response) => response.json())
+
+        .then((data) => {
+            console.log(data);
+        })
+
+}
+
+
+getAndParseJSON();
+3
